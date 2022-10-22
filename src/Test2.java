6a3c2d3d7c53c1d455fdef32f94b30cafa358193
Vicentevv <vicenteval759@gmail.com>
2022-10-21 22:56:32 -0500

    segunda carga de la versión

--- src/Test2.java
+++ src/Test2.java
@@ -1,2 +1,10 @@
 public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Temas de proyectos explicados en centro de convenciones");
+        System.out.println("Implementación de laboratirio multiusuario para practicas es un ser de TV");
+        System.out.println("Implementación de un videojuego para juventud idente");
+        System.out.println("Desarrollo de videojuego en el marco de la materia juego de la carrera de computación");
+        System.out.println("Filtros en realidad aumentada");
+    }
 }
