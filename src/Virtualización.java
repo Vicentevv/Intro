cd465edb592c34f689ceb2cf5e1affd356c72b67
Vicentevv <vicenteval759@gmail.com>
2022-10-21 22:42:59 -0500

    segunda carga de la versión

---
+++ src/Virtualización.java
@@ -0,0 +1,12 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Virtualización {
+
+    public static void main(String[] args) {
+        System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES");
+        System.out.println("");
+    }
+
+}
