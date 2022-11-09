50c05040e7588b9f53c5fe6990766be9e154843b
Vicentevv <116390036+Vicentevv@users.noreply.github.com>
2022-11-08 19:52:09 -0600

    Add files via upload

---
+++ CondicionalSimple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Programa para determinar si una persona es mayor de edad
+ */
+public class CondicionalSimple {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Programa para determinar mayoria de edad");
+        System.out.println("Ingrese su edad: ");
+        int edad = teclado.nextInt();
+        boolean evaluacion = (edad >= 18);
+        if (evaluacion)
+            System.out.println("Eres mayor de edad");
+            System.out.println("Estas casi listo para ir a la Universidad");
+    }
+}
