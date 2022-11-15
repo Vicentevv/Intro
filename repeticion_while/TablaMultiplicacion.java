d219a480464ab72fc0c51bbb2aa262bc5564e4ce
Vicentevv <116390036+Vicentevv@users.noreply.github.com>
2022-11-15 15:45:59 -0600

    Add files via upload

---
+++ repeticion_while/TablaMultiplicacion.java
@@ -0,0 +1,26 @@
+package repeticion_while;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Generar la tabla de multiplicar hasta el 10 de un número ingresado por el teclado
+ */
+public class TablaMultiplicacion {
+
+    public static void main(String[] args) {
+        //System.out.println("PROGRAMA PARA GENERAR LA TABLA DE MULTIPLICAR");
+        JOptionPane.showMessageDialog(null, "PROGRAMA PARA GENERAR LA TABLA DE MULTIPLICAR");
+        //System.out.println("Ingrese un número");
+        String num = JOptionPane.showInputDialog("Ingrese un número: ");
+        int contador = 1;
+        String acumulador = "";
+        while (contador <= 10) {
+            int mult = Integer.parseInt(num) * contador; // convertimos valor string a int
+            String salida = num + "x" + String.valueOf(contador) + " = " + String.valueOf(mult) + "\n";
+            acumulador = acumulador + salida;
+            contador = contador +1;
+        }
+        JOptionPane.showMessageDialog(null, acumulador);
+        }
+
+    }
