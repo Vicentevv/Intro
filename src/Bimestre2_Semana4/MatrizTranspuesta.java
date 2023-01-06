588ce7fa927bf6aa3638c6ee296ecccd9c198274
Vicentevv <116390036+Vicentevv@users.noreply.github.com>
2023-01-06 17:56:36 -0500

    Add files via upload

---
+++ src/Bimestre2_Semana4/MatrizTranspuesta.java
@@ -0,0 +1,23 @@
+package Bimestre2_Semana4;
+
+/**
+ *  Programa para generar la transpuesta de una matriz
+ */
+public class MatrizTranspuesta {
+    public static void main(String[] args) {
+        char letras [][] = {
+                {'a','b','c'},
+                {'d','e','f'},
+        }; // Dimensión 2x3
+        int filas = letras.length; // 2
+        int cols = letras[0].length; // 3
+        char transpuesta [][] = new char [cols][filas];
+        for (int fila = 0; fila < cols; fila++) {
+            for (int col = 0; col < filas; col++) {
+                transpuesta[fila][col] = letras [col][fila];
+                System.out.print(transpuesta[fila][col]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
